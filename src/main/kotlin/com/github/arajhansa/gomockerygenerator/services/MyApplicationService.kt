ff890366d45ecb72c9282ead26f0f73229228682
GitHub Action <action@github.com>
2021-04-04 18:04:02 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/arajhansa/gomockerygenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.arajhansa.gomockerygenerator.services
+
+import com.github.arajhansa.gomockerygenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/arajhansa/gomockerygenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/arajhansa/gomockerygenerator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.arajhansa.gomockerygenerator.services
 
+import com.github.arajhansa.gomockerygenerator.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
